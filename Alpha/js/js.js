efb7591986b27b0cc96d867d2bde2c536b3f76d1
JosselinTD <josselin.td@gmail.com>
2013-10-18 21:18:20 +0200

    Divers
    Avancement du travail

--- Alpha/js/js.js
+++ Alpha/js/js.js
@@ -9,6 +9,11 @@
 	var wn1 = document.getElementsByClassName("wn1");
 	var wn2 = document.getElementsByClassName("wn2");
 	var toMove;
+	var fonds = ["ajani", "liliana", "jace", "garruk", "chandra"];
+	var fondsButton = document.getElementById("fonds");
+	var basePV = 20;
+	var pvsButton = document.getElementById("pvs");
+	var resetButton = document.getElementById("button11");
 	
 	var j1c1 = document.getElementById("j1nb0"),
 		j1e1 = document.getElementById("j1nb0"), 
@@ -33,6 +38,19 @@
 	creditsButton.addEventListener("click", function(){
 		slidebox.slideNext();
 	});
+	pvsButton.addEventListener("click", function(){
+		if(basePV == 20){
+			basePV = 30;
+			document.getElementById("changePv").innerHTML = 20;
+		} else {
+			basePV = 20;
+			document.getElementById("changePv").innerHTML = 30;
+		}
+		
+		reset();
+	});
+	resetButton.addEventListener("click", reset);
+	fondsButton.addEventListener("click", loadFond);
 	joueur1.addEventListener("touchstart", startJ1, false);
 	joueur1.addEventListener("touchmove", moveJ1, false);
 	
@@ -42,13 +60,20 @@
 	joueur1.style.height = (window.innerHeight-50)/2 + "px";
 	joueur2.style.height = (window.innerHeight-50)/2 + "px";
 	
-	for(var i = 1; i<3; i++){
-		for(var j = 0; j<7; j++){
-			document.getElementById("j"+i+"nb"+j).style.left = (20*j)-20+"%";
+	loadFond();
+
+	function loadFond(){
+		var random1 = Math.floor(Math.random()*5);
+		var random2 = Math.floor(Math.random()*5);
+		while(random2 == random1){
+			random2 = Math.floor(Math.random()*5);
 		}
-		document.getElementById("j"+i+"nb0").style.display = "none";
-		document.getElementById("j"+i+"nb6").style.display = "none";
+
+		document.getElementById("img1").src = "img/"+fonds[random1]+".jpg";
+		document.getElementById("img2").src = "img/"+fonds[random2]+".jpg";
 	}
+	
+	reset();
 	
 	var pos;
 	function startJ1(evt){
@@ -151,5 +176,24 @@
 	function endJ1(evt){
 		evt.preventDefault();
 		
+	}
+	
+	function reset(){
+		for(var i = 1; i<3; i++){
+			for(var j = 0; j<7; j++){
+				document.getElementById("j"+i+"nb"+j).style.left = (20*j)-20+"%";
+				document.getElementById("j"+i+"nb"+j).innerHTML = basePV - 3 + j;
+			}
+			document.getElementById("j"+i+"nb0").style.display = "none";
+			document.getElementById("j"+i+"nb6").style.display = "none";
+		}
+		j1c1 = document.getElementById("j1nb0");
+		j1e1 = document.getElementById("j1nb0"); 
+		j1c2 = document.getElementById("j1nb6");
+		j1e2 = document.getElementById("j1nb6"); 
+		j2c1 = document.getElementById("j2nb0");
+		j2e1 = document.getElementById("j2nb0"); 
+		j2c2 = document.getElementById("j2nb6");
+		j2e2 = document.getElementById("j2nb6");
 	}
 });
